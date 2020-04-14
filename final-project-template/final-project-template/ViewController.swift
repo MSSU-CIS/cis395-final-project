808efe2a28e2971a5c39608f26a143d80edc176d
Otto Borchert <borchert-o@mssu.edu>
2020-04-14 10:48:48 -0500

    Initial blank project

---
+++ final-project-template/final-project-template/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  final-project-template
+//
+//  Created by Otto Borchert on 4/14/20.
+//  Copyright © 2020 Missouri Southern State University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
